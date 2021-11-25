a0e07151b90bafc96d116daca051d5cd7dca4242
Nathaniel Brost <NathanielBrost@NathaniBook2021.attlocal.net>
2021-11-24 19:43:01 -0600

    decide on steps taken for 4 selections

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 "use strict"
 
 console.log("Hello Nate!");
+
+// part 1, 2, 3, 4 will be similar. 
+// create arrays with options for places, restaurants, transport, entertainment
+// create prompt "Enter either 'Locale; Food; Vehicle; or Entertainment'"
+// create functions for each, to return a random result from these arrays
+// console log the results
+// 
