c15a340603f310ba7bbdbec6dcac96a25fc0203d
Nathaniel Brost <NathanielBrost@NathaniBook2021.attlocal.net>
2021-11-24 19:49:56 -0600

    add comments for steps for second part of proj

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ console.log("Hello Nate!");
 // create prompt "Enter either 'Locale; Food; Vehicle; or Entertainment'"
 // create functions for each, to return a random result from these arrays
 // console log the results
+// store these as an array
 // 
+// show prompt "These are your selections: blah, blah, blah, and blah. 
+// Are you happy with these results? C to Confirm or N if No"
+// If no, prompt asking "Which should we reselect at this moment? L, F, V, or E?"
+// When user input = C for Confirm, console log these results
